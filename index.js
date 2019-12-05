c10cd7a1ce43d035ec1166458b335b07ebaf9eac
Thara Viswanathan <tharabrindharani@gmail.com>
2019-12-05 22:58:04 +0530

    Added Students object

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-function showName(name) {
-    console.log(name)
-}
+var Class1 = {   'Student1': ['Viswanathan', 'Mohan'],
+                 'Student2': ['Sangu', 'VNMohan'],
+                 'Student3': ['Rathan', 'Viswanathan'],
+                 'Student4': ['Thara', 'Gopinath']};
+                 
+var Class2 = {   'Student1': {'FirstName': 'Viswanathan', 'LastName':'Mohan'},
+                 'Student2': {'FirstName': 'Sangu', 'LastName':'VNMohan'},
+                 'Student3': {'FirstName': 'Rathan', 'LastName':'Viswanathan'},
+                 'Student4': {'FirstName': 'Thara', 'LastName':'Gopinath'}};
+
 document.querySelector("#timebutton").addEventListener("click", function(){
   UpdateShowTime();
+  document.getElementById("sFirstName").innerHTML = Class1.Student2[0]
+  document.getElementById("sLastName").innerHTML = Class1.Student2[1]
+
+  document.getElementById("sFirstName").innerHTML = Class2.Student1.FirstName
+  document.getElementById("sLastName").innerHTML = Class2.Student1.LastName
+
   });
+
 
   
 function GetTime(){
@@ -11,9 +25,6 @@ function GetTime(){
   return today.toLocaleTimeString();
 }
 
-  function UpdateShowTime(){
+function UpdateShowTime(){
     document.getElementById("ShowTime").innerHTML = GetTime();
   }
-
-
-showName('Sangamithirai')
