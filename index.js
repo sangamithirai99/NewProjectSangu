09b94aa68bcca134f143934a710144428c6256eb
Thara Viswanathan <tharabrindharani@gmail.com>
2019-12-05 15:16:37 +0530

    added showtime

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ function showName(name) {
     console.log(name)
 }
 document.querySelector("#timebutton").addEventListener("click", function(){
-    document.getElementById("timebutton").innerHTML = "xxxx";
+  UpdateShowTime();
   });
+
+  
+function GetTime(){
+  var today = new Date();
+  return today.toLocaleTimeString();
+}
+
+  function UpdateShowTime(){
+    document.getElementById("ShowTime").innerHTML = GetTime();
+  }
+
 
 showName('Sangamithirai')
